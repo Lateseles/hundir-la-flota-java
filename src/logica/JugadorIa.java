3cf48d9f0ea6ceef80d64f35550e412c01167e35
lateseles <vevases@gmail.com>
2026-01-17 19:37:15 +0100

    Agregada la funcionalidad basica de la ia

--- src/logica/JugadorIa.java
+++ src/logica/JugadorIa.java
@@ -1,21 +1,42 @@
 package logica;
 
+import java.util.Random;
+
 import logica.espacio.Coordenada;
+import logica.espacio.Orientacion;
+import logica.espacio.Posicion;
 
 public class JugadorIa extends Jugador{
+    private Random generadorAleatorio;
 
     protected JugadorIa(IEntradaSalida gui) {
         super(gui);        
+        generadorAleatorio = new Random();
     }
 
     @Override
     public void agregarBarco(String nombre, byte longitud) {
-        //TODO IMPLEMENTAR METODO
+        Barco barco;
+        Posicion posicion;
+
+        do{
+            posicion = escogerUbicacion(nombre, longitud);
+            barco = new Barco(nombre, posicion, longitud);            
+        }while(!isValido(barco));
+
+        barcos.add(barco);
     }
 
     @Override
     public Coordenada dispara(Tablero accion) {
-        return null; //TODO IMPLEMENTAR METODO
+        return new Coordenada((byte)(Math.random() * 10), (byte)(Math.random() * 10));
+    }
+
+    private Posicion escogerUbicacion(String nombre, byte longitud){        
+        return new Posicion(
+                   new Coordenada((byte)(generadorAleatorio.nextInt(Tablero.TOTAL_FILAS)),
+                                  (byte)(generadorAleatorio.nextInt(Tablero.TOTAL_COLUMNAS))),
+                   Orientacion.values()[generadorAleatorio.nextInt(Orientacion.values().length)]);
     }
 
 }
