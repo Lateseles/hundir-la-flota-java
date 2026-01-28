9503d57a418315b3a9a6bfcb630ab603be59508a
lateseles <vevases@gmail.com>
2026-01-28 22:30:56 +0100

    Agregados los comentarios de HundirFlota

--- src/HundirFlota.java
+++ src/HundirFlota.java
@@ -1,8 +1,13 @@
+/*  Proyecto desarrollado por:
+    Alexandra Ramón Angrisano y Francisco Javier Vera Valenciano
+    Entornos de desarrollo. 1ºDAW
+*/
+
 import logica.Juego;
 
 public class HundirFlota {
     public static void main(String[] args) {
-        Juego juego = new Juego(new Interfaz());
+        Juego juego = new Juego(new Interfaz()); //Con esta interfaz se mostrará el juego por consola
         juego.jugar();
     }
 }
